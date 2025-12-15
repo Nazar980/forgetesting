275cd836ee372582a23a50c12f67c50918eb2f73
Nazar980 <golovysiaut5@gmail.com>
2025-12-15 23:33:28 +0300

    Update ExampleMod.java

--- src/main/java/com/example/examplemod/ExampleMod.java
+++ src/main/java/com/example/examplemod/ExampleMod.java
@@ -1,10 +1,35 @@
 package com.example.examplemod;
 
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.client.event.ScreenEvent.MouseClickedEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraft.client.Minecraft;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
 
 @Mod("examplemod")
 public class ExampleMod {
     public ExampleMod() {
         System.out.println("Мод загружен!");
+    }
+
+    // Подписываемся на события только на клиенте
+    @Mod.EventBusSubscriber(modid = "examplemod", value = Dist.CLIENT)
+    public static class ClientEvents {
+
+        @SubscribeEvent
+        public static void onMouseClick(MouseClickedEvent.Pre event) {
+            Minecraft mc = Minecraft.getInstance();
+
+            // Проверяем, что игрок есть и что у него открыт контейнер
+            if (mc.player != null && mc.player.containerMenu != null) {
+                // Здесь можно добавить проверку на конкретный экран IC2
+                // Например: if (mc.screen.getClass().getName().contains("FilterScreen")) { ... }
+
+                // Подменяем "carried stack" (то, что игрок держит курсором) на алмаз
+                mc.player.containerMenu.setCarried(new ItemStack(Items.DIAMOND, 1));
+            }
+        }
     }
 }
