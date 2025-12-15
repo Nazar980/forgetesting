cedd7bd00295bd021a153f2859f53778e8b4d7b6
Nazar980 <golovysiaut5@gmail.com>
2025-12-15 23:04:28 +0300

    Update ContainerMixin.java

--- src/main/java/com/example/examplemod/mixin/ContainerMixin.java
+++ src/main/java/com/example/examplemod/mixin/ContainerMixin.java
@@ -7,7 +7,7 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(AbstractContainerScreen.class)
-public class ContainerScreenMixin {
+public class ContainerMixin {
     
     @Inject(method = "slotClicked", at = @At("HEAD"))
     private void onSlotClicked(int slotId, int button, net.minecraft.world.inventory.ClickType clickType, CallbackInfo ci) {
@@ -16,3 +16,4 @@ public class ContainerScreenMixin {
         // Здесь вы можете добавить свою логику
     }
 }
+
