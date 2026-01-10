32580799ce37f23eb45b7ff1be3d3773c855b5f1
Nazar980 <golovysiaut5@gmail.com>
2026-01-10 15:03:05 +0300

    Update ExampleMod.java

--- src/main/java/edu/unl/csce466/ExampleMod.java
+++ src/main/java/edu/unl/csce466/ExampleMod.java
@@ -1,27 +1,16 @@
 package edu.unl.csce466;
 
 import com.mojang.logging.LogUtils;
-import net.minecraft.client.Minecraft;
-import net.minecraft.network.chat.Component;
-import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.item.*;
-import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.Blocks;
-import net.minecraft.world.level.block.state.BlockBehaviour;
-import net.minecraft.world.level.material.Material;
-import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.client.event.InputEvent;
-import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.server.ServerStartingEvent;
-import net.minecraftforge.eventbus.api.IEventBus;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
-import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import net.minecraftforge.items.ItemHandlerHelper;
-import net.minecraftforge.registries.DeferredRegister;
-import net.minecraftforge.registries.ForgeRegistries;
-import net.minecraftforge.registries.RegistryObject;
-import org.lwjgl.glfw.GLFW;
 import org.slf4j.Logger;
+
+@Mod(ExampleMod.MODID)
+public class ExampleMod {
+
+    public static final String MODID = "examplemod";
+    private static final Logger LOGGER = LogUtils.getLogger();
+
+    public ExampleMod() {
+        LOGGER.info("ExampleMod loaded.");
+    }
+}
